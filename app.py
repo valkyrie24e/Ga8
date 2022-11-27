0b277a3b8eae2a98d69d6180a28dfea5628dd04c
valkyrie24e <96327461+valkyrie24e@users.noreply.github.com>
2022-11-27 15:24:02 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+st.header('Divide Two Numbers')
+def user_input_features():
+name1 = st.munber_input("Enter the first number", "Type Here ...")
+name2 = st.number_input("Enter the second number", "Type Here ...")
+Getanswer = st.selectbox("Get answer in:",['whole', 'Float'])
+
+if(status == 'Whole'):
+    # take height input in centimeters
+  try:
+        answer = round(name1/name2)
+  except:
+        st.text("Enter the numbers")
+ 
+else:
+    # take height input in meters
+  try:
+        answer = float(name1/name2)
+  except:
+        st.text("Enter the numbers")
+if(st.button("submit")):
+   st.text("The answer is {}.".format(answer))
+     data = {'Enter the first number': name1,
+            'Enter the second number': name2,
+            'Get answer in': Getanswer,
+            }
+    features = pd.DataFrame(data, index=[0])
+    return features
+  
